29241a1a026046b50576f2d268932cf50db2f844
Uvaish <uvaishraza000@gmail.com>
2026-04-23 10:49:44 -0500

    fixed register bug and cors issue

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -111,7 +111,7 @@ export const login = async (req, res) => {
         return res.status(200).cookie("token", token, {
             maxAge: 1 * 24 * 60 * 60 * 1000,
             httpOnly: true,
-            sameSite: "Strict",
+            sameSite: "None",
         })
             .json({
                 message: `Welcome back ${user.fullname}`,
