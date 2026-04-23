80933ba8acd7cb443bfce98a7c93e63628eb5fbf
Uvaish <uvaishraza000@gmail.com>
2026-04-23 15:05:14 -0500

    final fixes: register, cloudinary, cors, cookies

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 const corsOptions = {
     origin: [
         "http://localhost:5173",
-        "https://job-jodi-job-portal-app.vercel.app/"
+        "https://job-jodi-job-portal-app.vercel.app"
     ],
     credentials: true,
 };
